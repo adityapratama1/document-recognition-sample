7cc073decf3dc27b2ca8e73f6ba23eac18659860
Aditya Pratama <pratamaadetiya@gmail.com>
2019-07-28 17:25:00 +0700

    DocumentRecognition

---
+++ app/src/main/java/com/bms/document/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bms.document;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bms.documentrecognition.engine.DocumentResults;
+import com.bms.documentrecognition.recognition.DocumentRecognition;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button scan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan = (Button) findViewById(R.id.scan_button);
+        scan.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.scan_button){
+            DocumentRecognition document = new DocumentRecognition(this, "pRBXmfpd59w EvoUSGN5FsWLNfxi4CN g3jps4sHdIV1");
+            document.initiateScan();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        DocumentResults results = DocumentRecognition.parseActivityResult(requestCode,resultCode,data);
+        Toast.makeText(this, results.getLines(), Toast.LENGTH_LONG).show();
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
